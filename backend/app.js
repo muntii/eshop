5732ba64db6846ecd64a8bef718a6509d3dc7a82
Muntajib <muntajibkhan14@gmail.com>
2024-11-23 15:05:02 +0500

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,13 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors({
-  origin: ['https://eshop-client-zeta.vercel.app',],
-  credentials: true
-}));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin: https://eshop-client-zeta.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
