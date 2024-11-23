16ae058849c148b74144ba5978a4c4b3d14cb682
Muntajib <muntajibkhan14@gmail.com>
2024-11-23 15:26:19 +0500

    update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 
-export const server = "https://eshop-ashy.vercel.app/api/v2";
-export const backend_url = "http://localhost:8000/";
+export const server = "https://eshop-client-zeta.vercel.app/api/v2";
+export const backend_url = "https://eshop-ashy.vercel.app";
 
