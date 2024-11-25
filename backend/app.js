e8a905459dbb6b86009f38d5087ee2291c90feef
muntii <62630609+muntii@users.noreply.github.com>
2024-11-25 09:44:45 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin": "https://eshop-client-zeta.vercel.app");
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+
 
 app.use(express.json());
 app.use(cookieParser());
