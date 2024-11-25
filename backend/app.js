84fb29ac3f2e6a6fc1c9177a19c4fa412ab0b7ba
muntii <62630609+muntii@users.noreply.github.com>
2024-11-25 10:39:51 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://eshop-client-zeta.vercel.app");
   res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
   next();
 });
 
